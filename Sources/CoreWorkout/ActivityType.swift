545567c703b9e3cfb86fd69421079a0bb0a46eee
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-04-21 21:21:07 -0400

    Add `ActivityType`

---
+++ Sources/CoreWorkout/ActivityType.swift
@@ -0,0 +1,249 @@
+//
+//  ActivityType.swift
+//  CoreWorkout
+//
+//  Created by Alsey Coleman Miller on 4/21/25.
+//
+
+/// Fitness Activity Type
+public enum ActivityType: String, Codable, CaseIterable, Sendable {
+    
+    /// American Football activity
+    case americanFootball = "american.football"
+    
+    /// Archery activity
+    case archery = "archery"
+    
+    /// Australian Football activity
+    case australianFootball = "australian.football"
+    
+    /// Badminton activity
+    case badminton = "badminton"
+    
+    /// Baseball activity
+    case baseball = "baseball"
+    
+    /// Basketball activity
+    case basketball = "basketball"
+    
+    /// Bowling activity
+    case bowling = "bowling"
+    
+    /// Boxing activity
+    case boxing = "boxing"
+    
+    /// Climbing activity
+    case climbing = "climbing"
+    
+    /// Cricket activity
+    case cricket = "cricket"
+    
+    /// Cross Training activity
+    case crossTraining = "cross.training"
+    
+    /// Curling activity
+    case curling = "curling"
+    
+    /// Cycling activity
+    case cycling = "cycling"
+    
+    /// Dance activity
+    case dance = "dance"
+    
+    /// Dance Inspired Training activity
+    case danceInspiredTraining = "dance.inspired.training"
+    
+    /// Elliptical activity
+    case elliptical = "elliptical"
+    
+    /// Equestrian Sports activity
+    case equestrianSports = "equestrian.sports"
+    
+    /// Fencing activity
+    case fencing = "fencing"
+    
+    /// Fishing activity
+    case fishing = "fishing"
+    
+    /// Functional Strength Training activity
+    case functionalStrengthTraining = "functional.strength.training"
+    
+    /// Golf activity
+    case golf = "golf"
+    
+    /// Gymnastics activity
+    case gymnastics = "gymnastics"
+    
+    /// Handball activity
+    case handball = "handball"
+    
+    /// Hiking activity
+    case hiking = "hiking"
+    
+    /// Hockey activity
+    case hockey = "hockey"
+    
+    /// Hunting activity
+    case hunting = "hunting"
+    
+    /// Lacrosse activity
+    case lacrosse = "lacrosse"
+    
+    /// Martial Arts activity
+    case martialArts = "martial.arts"
+    
+    /// Mind and Body activity
+    case mindAndBody = "mind.and.body"
+    
+    /// Mixed Metabolic Cardio Training activity
+    case mixedMetabolicCardioTraining = "mixed.metabolic.cardio.training"
+    
+    /// Paddle Sports activity
+    case paddleSports = "paddle.sports"
+    
+    /// Play activity
+    case play = "play"
+    
+    /// Preparation and Recovery activity
+    case preparationAndRecovery = "preparation.and.recovery"
+    
+    /// Racquetball activity
+    case racquetball = "racquetball"
+    
+    /// Rowing activity
+    case rowing = "rowing"
+    
+    /// Rugby activity
+    case rugby = "rugby"
+    
+    /// Running activity
+    case running = "running"
+    
+    /// Sailing activity
+    case sailing = "sailing"
+    
+    /// Skating Sports activity
+    case skatingSports = "skating.sports"
+    
+    /// Snow Sports activity
+    case snowSports = "snow.sports"
+    
+    /// Soccer activity
+    case soccer = "soccer"
+    
+    /// Softball activity
+    case softball = "softball"
+    
+    /// Squash activity
+    case squash = "squash"
+    
+    /// Stair Climbing activity
+    case stairClimbing = "stair.climbing"
+    
+    /// Surfing Sports activity
+    case surfingSports = "surfing.sports"
+    
+    /// Swimming activity
+    case swimming = "swimming"
+    
+    /// Table Tennis activity
+    case tableTennis = "table.tennis"
+    
+    /// Tennis activity
+    case tennis = "tennis"
+    
+    /// Track and Field activity
+    case trackAndField = "track.and.field"
+    
+    /// Traditional Strength Training activity
+    case traditionalStrengthTraining = "traditional.strength.training"
+    
+    /// Volleyball activity
+    case volleyball = "volleyball"
+    
+    /// Walking activity
+    case walking = "walking"
+    
+    /// Water Fitness activity
+    case waterFitness = "water.fitness"
+    
+    /// Water Polo activity
+    case waterPolo = "water.polo"
+    
+    /// Water Sports activity
+    case waterSports = "water.sports"
+    
+    /// Wrestling activity
+    case wrestling = "wrestling"
+    
+    /// Yoga activity
+    case yoga = "yoga"
+    
+    /// Barre activity
+    case barre = "barre"
+    
+    /// Core Training activity
+    case coreTraining = "core.training"
+    
+    /// Cross Country Skiing activity
+    case crossCountrySkiing = "cross.country.skiing"
+    
+    /// Downhill Skiing activity
+    case downhillSkiing = "downhill.skiing"
+    
+    /// Flexibility activity
+    case flexibility = "flexibility"
+    
+    /// High Intensity Interval Training activity
+    case highIntensityIntervalTraining = "high.intensity.interval.training"
+    
+    /// Jump Rope activity
+    case jumpRope = "jump.rope"
+    
+    /// Kickboxing activity
+    case kickboxing = "kickboxing"
+    
+    /// Pilates activity
+    case pilates = "pilates"
+    
+    /// Snowboarding activity
+    case snowboarding = "snowboarding"
+    
+    /// Stairs activity
+    case stairs = "stairs"
+    
+    /// Step Training activity
+    case stepTraining = "step.training"
+    
+    /// Wheelchair Walk Pace activity
+    case wheelchairWalkPace = "wheelchair.walk.pace"
+    
+    /// Wheelchair Run Pace activity
+    case wheelchairRunPace = "wheelchair.run.pace"
+    
+    case taiChi = "tai.chi"
+    
+    case mixedCardio = "mixed.cardio"
+    
+    case handCycling = "hand.cycling"
+    
+    case discSports = "disc.sports"
+    
+    case fitnessGaming = "fitness.gaming"
+    
+    case cardioDance = "cardio.dance"
+    
+    case socialDance = "social.dance"
+    
+    case pickleball = "pickleball"
+    
+    case cooldown = "cooldown"
+    
+    case swimBikeRun = "swim.bike.run"
+    
+    case transition = "transition"
+    
+    case underwaterDiving = "underwater.diving"
+    
+    case other = "other"
+}
